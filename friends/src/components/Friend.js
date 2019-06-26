38db729bf011e8f4ae23d8093904f9f846f52249
Matt Hagner <web@matthagner.com>
2019-06-25 19:23:50 -0500

    Update Friend component

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function Friend({ name, age, email }) {
   return (
-    <li>
-      <h2>{name}</h2> {age} - {email}
-    </li>
+    <>
+      <h2>Name: {name}</h2>
+      <h3>Age: {age}</h3>
+      <h4>Email: {email}</h4>
+    </>
   );
 }
