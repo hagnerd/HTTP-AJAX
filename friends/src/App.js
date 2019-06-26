6a929bb715bc4534a60cac690b652a718636267b
Matt Hagner <web@matthagner.com>
2019-06-25 19:25:11 -0500

    Add home path

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import { Route } from "react-router-dom";
+import FriendList from "./components/FriendList";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/friends").then(({ data }) => {
+      this.setState({ friends: data });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+
+        <Route
+          path="/"
+          exact
+          render={props => (
+            <FriendList {...props} friends={this.state.friends} />
+          )}
+        />
+
+      </div>
+    );
+  }
 }
 
 export default App;
