e28ff3509ca972d5e43b0cf4b62e330c7682a9b8
Matt Hagner <web@matthagner.com>
2019-06-25 20:21:33 -0500

    Make styling changes to Friend

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -2,10 +2,16 @@ import React from "react";
 
 export default function Friend({ name, age, email }) {
   return (
-    <>
-      <h2>Name: {name}</h2>
-      <h3>Age: {age}</h3>
-      <h4>Email: {email}</h4>
-    </>
+    <div className="flex flex-col items-center bg-white m-3 p-5 rounded">
+      <h2 className="text-2xl text-gray-700">
+        <span className="font-bold">Name:</span> {name}
+      </h2>
+      <h3 className="text-lg">
+        <span className="font-bold">Age:</span> {age}
+      </h3>
+      <h4>
+        <span className="font-bold">Email:</span> {email}
+      </h4>
+    </div>
   );
 }
